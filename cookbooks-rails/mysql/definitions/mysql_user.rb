6d261da5391a5ad7b0981c39810c5d6fc15bd685
Iskander Haziev <gvalmon@gmail.com>
2013-07-21 01:32:11 +0400

    added newrelic mysql plugin support

--- cookbooks-rails/mysql/definitions/mysql_user.rb
+++ cookbooks-rails/mysql/definitions/mysql_user.rb
@@ -3,25 +3,22 @@ define :mysql_user do
   mysql_cmd = "mysql -uroot -p#{root_password} mysql -e"
 
   access_from = node[:deploy_user][:database_access_from] || []
-  access_from += ['localhost']
-
-  sql = [params[:name], "IDENTIFIED BY '#{params[:password]}'"].join(' ')
-
-  exists = ["#{mysql_cmd} \"SELECT user FROM user WHERE user='#{params[:name]}';\""]
-  exists.push "| grep #{params[:name]}"
-  exists = exists.join ' '
+  access_from += ['localhost', node['mysql']['bind_address']]
 
   access_from.each do |host|
-    execute "altering mysql user #{params[:name]}" do
-      user "root"
-      command "#{mysql_cmd} \"SET PASSWORD FOR '#{params[:name]}'@'#{host}' = PASSWORD('#{params[:password]}');\""
-      only_if exists, user: "root"
-    end
+    exists = ["#{mysql_cmd} \"SELECT user,host FROM mysql.user  WHERE user='#{params[:name]}';\""]
+    exists.push "| grep #{params[:name]} | grep #{host}"
+    exists = exists.join ' '
 
     execute "creating mysql user #{params[:name]}" do
       user "root"
       command "#{mysql_cmd} \"CREATE USER '#{params[:name]}'@'#{host}' IDENTIFIED BY '#{params[:password]}';\""
       not_if exists, user: "root"
+    end
+
+    execute "altering mysql user #{params[:name]}" do
+      user "root"
+      command "#{mysql_cmd} \"SET PASSWORD FOR '#{params[:name]}'@'#{host}' = PASSWORD('#{params[:password]}');\""
     end
 
     execute "grant priveligies for mysql user #{params[:name]}" do
--- cookbooks-rails/newrelic/recipes/nginx.rb
+++ cookbooks-rails/newrelic/recipes/nginx.rb
@@ -1,3 +1,57 @@
-newrelic_plugin do
-  plugin "nginx"
+name = "nginx"
+deploy_user = node[:deploy_user][:username]
+tmp_dir = "/home/#{deploy_user}/downloads"
+plugins_dir = node[:newrelic][:plugins_dir]
+agent_dir = File.join(plugins_dir, "newrelic_#{name}_agent")
+
+rbenv_gem "newrelic_plugin" do
+  package_name    "newrelic_plugin"
+  user            deploy_user
+  rbenv_version   rbenv_version node[:deploy_user][:ruby_version]
+end
+
+directory "#{plugins_dir}" do
+  mode 0755
+  owner deploy_user
+  group deploy_user
+  recursive true
+  action :create
+end
+
+unless File.exists? "#{agent_dir}/config/newrelic_plugin.yml"
+  cookbook_file "#{tmp_dir}/newrelic_#{name}_agent.gz" do
+    owner deploy_user
+    group deploy_user
+    mode 0755
+  end
+  bash "extract plugin" do
+    code "sudo -u #{deploy_user} tar -xzf #{tmp_dir}/newrelic_#{name}_agent.gz -C #{plugins_dir}"
+  end
+end
+
+template "#{agent_dir}/config/newrelic_plugin.yml" do
+  source "newrelic_#{name}_plugin.yml.erb"
+  owner deploy_user
+  group deploy_user
+  mode "640"
+  variables(:license_key => node[:newrelic][:license_key])
+  notifies(:restart, "service[newrelic-#{name}]") if node[:newrelic][:enabled]
+end
+
+directory "#{agent_dir}/tmp" do
+  mode 0755
+  owner deploy_user
+  group deploy_user
+  action :create
+end
+
+service "newrelic-#{name}" do
+  service_name "newrelic-#{name}"
+  enabled true
+  running true
+  restart_command "sudo -u #{deploy_user} -i #{agent_dir}/newrelic_#{name}_agent.daemon restart"
+  start_command "sudo -u #{deploy_user} -i #{agent_dir}/newrelic_#{name}_agent.daemon start"
+  stop_command "sudo -u #{deploy_user} -i #{agent_dir}/newrelic_#{name}_agent.daemon stop"
+  supports [ :start, :stop, :restart ]
+  action [ :nothing ]
 end
