e7c06afd2bd1f02252566282a5d0dce09c7f9d95
Iskander Haziev <gvalmon@gmail.com>
2013-09-14 18:06:27 +0400

    better iptables configuration

--- cookbooks-rails/security/recipes/default.rb
+++ cookbooks-rails/security/recipes/default.rb
@@ -1,14 +1,14 @@
-template "/etc/iptables.sh" do
-  source "iptables.sh.erb"
+template "/etc/iptables.rules" do
+  source "iptables.rules.erb"
   owner  "root"
   group  "root"
   mode   "0700"
 end
-bash "sh /etc/iptables.sh"
-
-template "/etc/rc.local" do
-  source "rc.local"
-  owner  "root"
-  group  "root"
-  mode   "0644"
+bash "iptables-restore < /etc/iptables.rules"
+bash "iptables.rules to interfaces" do
+  user "root"
+  code <<-EOF
+    echo "pre-up iptables-restore < /etc/iptables.rules" >> /etc/network/interfaces
+  EOF
+  not_if "cat /etc/network/interfaces | grep iptables.rules"
 end
