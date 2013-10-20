2b84aba389af18d5b4bf43904f99081a69dac1c8
Iskander Haziev <gvalmon@gmail.com>
2013-10-20 14:56:22 +0400

    better syntax for security ports

--- cookbooks-rails/security/attributes/default.rb
+++ cookbooks-rails/security/attributes/default.rb
@@ -1,1 +1,2 @@
-default[:security][:additional_ports] = []
+default[:security][:default_ports] = {'22': 'all', '80': 'all', '443': 'all'}
+default[:security][:ports] = {}
--- cookbooks-rails/security/recipes/default.rb
+++ cookbooks-rails/security/recipes/default.rb
@@ -1,9 +1,17 @@
+if node[:security][:additional_ports].any?
+  raise "security[additional_ports] is deprecated. please use security[ports] with reverted hash syntax."
+end
+
+ports = node[:security][:default_ports].merge(node[:security][:ports])
+
 template "/etc/iptables.rules" do
   source "iptables.rules.erb"
   owner  "root"
   group  "root"
   mode   "0700"
+  variables(ports: ports)
 end
+
 bash "iptables-restore < /etc/iptables.rules"
 bash "iptables.rules to interfaces" do
   user "root"
