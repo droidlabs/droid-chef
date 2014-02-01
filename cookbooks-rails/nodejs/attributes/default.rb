babe8e04e66f5c07195e10d645d7ee7c9ac33d85
Iskander Haziev <gvalmon@gmail.com>
2014-02-02 01:25:53 +0400

    install node.js as deploy user

--- cookbooks-rails/nodejs/attributes/default.rb
+++ cookbooks-rails/nodejs/attributes/default.rb
@@ -1,4 +1,4 @@
-default["nodejs"]["version"]          = "0.10.5"
+default["nodejs"]["version"]          = "0.10.25"
 default["nodejs"]["dir"]              = "/usr/local"
 default["nodejs"]["url"]              = "http://nodejs.org/dist/v#{node["nodejs"]["version"]}/node-v#{node["nodejs"]["version"]}.tar.gz"
-default["nodejs"]["checksum"]         = "d64a7f2ab8f4419a11bde5379d6065666fd1cc4593ec828cb5ac57385efafa45"
+default["nodejs"]["checksum"]         = "1e330b4fbb6f7bb858a0b37d8573dd4956f40885"
--- cookbooks-rails/nodejs/recipes/default.rb
+++ cookbooks-rails/nodejs/recipes/default.rb
@@ -14,9 +14,11 @@ end
 
 tmp_dir = "/home/#{node[:deploy_user][:username]}/downloads"
 bash "install-node" do
+  user node[:deploy_user][:username]
+  group node[:deploy_user][:username]
   cwd tmp_dir
   code <<-EOH
-    tar -xzf node-v#{node["nodejs"]["version"]}.tar.gz
+    tar -xzf node-v#{node["nodejs"]["version"]}.tar.gz &&
     (cd node-v#{node["nodejs"]["version"]} && ./configure --prefix=#{node["nodejs"]["dir"]} && make && make install)
   EOH
   action :nothing
@@ -25,8 +27,15 @@ end
 
 "Node.js - download #{tmp_dir}/node-v#{node["nodejs"]["version"]}.tar.gz"
 remote_file "#{tmp_dir}/node-v#{node["nodejs"]["version"]}.tar.gz" do
+  owner node[:deploy_user][:username]
+  group node[:deploy_user][:username]
   source node["nodejs"]["url"]
   checksum node["nodejs"]["checksum"]
   notifies :run, resources(:bash => "install-node"), :immediately
   action :create_if_missing
+end
+
+execute "install npm" do
+  user node[:deploy_user][:username]
+  command "curl https://npmjs.org/install.sh | sudo -u #{node[:deploy_user][:username]} -i sh"
 end
