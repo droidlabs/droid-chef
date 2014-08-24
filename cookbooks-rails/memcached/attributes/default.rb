068216784583cb126a5e2d7555c277317ee3d5fe
Iskander Haziev <gvalmon@gmail.com>
2014-08-25 03:14:08 +0400

    fixes

--- cookbooks-rails/memcached/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default["memcached"]["port"]            = 11211
-default["memcached"]["listen"]          = "127.0.0.1"
-default["memcached"]["log_file"]        = "/var/log/memcached.log"
-default["memcached"]["max_memory"]      = 64
-default["memcached"]["max_connections"] = 1024
