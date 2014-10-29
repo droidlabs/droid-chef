0c636d703754aabafde270ebdcf39b4dc795ebc7
fadeev2010 <fadeev2010@gmail.com>
2014-10-30 02:24:16 +0300

    rubo fix warn

--- cookbooks-rails/rails/recipes/default.rb
+++ cookbooks-rails/rails/recipes/default.rb
@@ -1,18 +1,19 @@
 deploy_username = node[:deploy_user][:username]
 
-node[:rails][:packages].collect do |pkg|
+node[:rails][:packages].map do |pkg|
   package pkg
 end
 
 # create applications directory
-directory "/data/" do
+directory '/data/' do
   owner deploy_username
   group deploy_username
-  mode "0771" # for read app_users
+  mode '0771' # for read app_users
   action :create
 end
 
-using_port = 3010
+# using_port = 3010  Rubo: Useless assignment to variable - using_port.
+
 # setup for each application
 node[:applications].each_with_index do |app, index|
   node.run_state[:current_app] = app
