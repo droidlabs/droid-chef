9f8876a4d238afdc5702c9dcdc73fcf03e6ea367
fadeev2010 <fadeev2010@gmail.com>
2014-11-08 11:38:25 +0300

    elastic user /bin/false

--- cookbooks-rails/droid-elasticsearch/recipes/default.rb
+++ cookbooks-rails/droid-elasticsearch/recipes/default.rb
@@ -1,10 +1,23 @@
 # Wrapper-cookbook elasticsearch
 
+# Execute default recipe
 include_recipe 'elasticsearch::default'
 
+# Change shell for elsticsearch user 
+user node.elasticsearch[:user] do
+  comment "ElasticSearch User"
+  home    "#{node.elasticsearch[:dir]}/elasticsearch"
+  shell   "/bin/false"
+  uid     node.elasticsearch[:uid]
+  gid     node.elasticsearch[:user]
+  supports :manage_home => false
+  action  :modify
+  system true
+end
+
 # Make sure the service is started
-#
 service("elasticsearch") { action :start }
 
-# USE AFTER START ELASTICSEARCH !
+# Add monit cfg for elastic service
+# Use after start elasticsearch and monit service !
 include_recipe 'elasticsearch::monit'
