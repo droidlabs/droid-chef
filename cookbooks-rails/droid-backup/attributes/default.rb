d96c702ef34adeb0541ae4a364e3f940984a66a6
fadeev2010 <fadeev2010@gmail.com>
2014-11-17 13:36:06 +0300

    fix backup

--- cookbooks-rails/droid-backup/attributes/default.rb
+++ cookbooks-rails/droid-backup/attributes/default.rb
@@ -1,6 +1,8 @@
 # DROID ATTRIBUTES FOR BACKUPS
 
+version = node[:deploy_user][:ruby_version]
+
 default[:backup][:cron_hour]   = '0,12'
 default[:backup][:mailto]      = 'sample@example.com'
 default[:backup][:base_dir]    = '/opt/backup'
-default[:backup][:gem_bin_dir] = '/usr/local/rbenv/shims'
+default[:backup][:gem_bin_dir] = "sudo -i env RBENV_VERSION=#{version} /usr/local/rbenv/shims"
