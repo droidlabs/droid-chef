6dd25bcc15ab6fb8036cb3b38b8a67324750e18c
fadeev2010 <fadeev2010@gmail.com>
2014-11-28 16:39:38 +0300

    chg deploy_user

--- cookbooks-rails/deployer_user/definitions/app_user.rb
+++ cookbooks-rails/deployer_user/definitions/app_user.rb
@@ -2,13 +2,14 @@
 
 define :app_user do
 
-  # Chef::Log.debug("# create deploy group and user with name: #{params[:name]}")
+  # Chef::Log.debug("# create deploy group and user with name: #{params_name}")
+  params_name = params[:name]  
 
-  group params[:name]
-  user params[:name] do
+  group params_name
+  user params_name do
     comment 'Deploy User'
-    home "/home/#{params[:name]}"
-    gid params[:name]
+    home "/home/#{params_name}"
+    gid params_name
     shell '/bin/bash'
     password params[:password]
     supports manage_home: true
@@ -16,25 +17,25 @@ define :app_user do
 
   # Chef::Log.info("# params[:deploy_username1] = #{params[:deploy_username1]}")
 
-  group params[:name] do
-    members ["#{params[:name]}", "#{node[:deploy_user][:username]}"]
+  group params_name do
+    members ["#{params_name}", "#{node[:deploy_user][:username]}"]
     action :create
   end
 
 
-  #Chef::Log.debug("generate ssh keys for: #{params[:name]}")
+  #Chef::Log.debug("generate ssh keys for: #{params_name}")
 
-  execute "generate ssh keys for: #{params[:name]}." do
-    user params[:name]
-    creates "/home/#{params[:name]}/.ssh/id_rsa.pub"
-    command "ssh-keygen -t rsa -q -f /home/#{params[:name]}/.ssh/id_rsa -P \"\""
+  execute "generate ssh keys for: #{params_name}." do
+    user params_name
+    creates "/home/#{params_name}/.ssh/id_rsa.pub"
+    command "ssh-keygen -t rsa -q -f /home/#{params_name}/.ssh/id_rsa -P \"\""
   end
 
   keys = ["github.com", "bitbucket.org"].map { |h| `ssh-keyscan -H -trsa,dsa -p 22 #{h}` }
-  hosts_path = "/home/#{params[:name]}/.ssh/known_hosts"
+  hosts_path = "/home/#{params_name}/.ssh/known_hosts"
   file "ssh_known_hosts" do
-    user params[:name]
-    group params[:name]
+    user params_name
+    group params_name
     path hosts_path
     action :create
     content "#{keys.join($/)}#{$/}"
@@ -42,16 +43,16 @@ define :app_user do
   end
 
   # create ".bash_profile"
-  file "/home/#{params[:name]}/.bash_profile" do
-    user params[:name]
-    group params[:name]
+  file "/home/#{params_name}/.bash_profile" do
+    user params_name
+    group params_name
     action :touch
   end
 
   # directory for file downloads
-  directory "/home/#{params[:name]}/downloads" do
-    user params[:name]
-    group params[:name]
+  directory "/home/#{params_name}/downloads" do
+    user params_name
+    group params_name
     action :create
   end
 
