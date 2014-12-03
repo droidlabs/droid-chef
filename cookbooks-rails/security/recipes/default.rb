c70ed11cdb06cd5b51bb85a25a61a89310fe3691
fadeev2010 <fadeev2010@gmail.com>
2014-12-03 14:19:43 +0300

    fix security, start iptable rule

--- cookbooks-rails/security/recipes/default.rb
+++ cookbooks-rails/security/recipes/default.rb
@@ -1,20 +1,22 @@
-# if node[:security][:additional_ports] && node[:security][:additional_ports].any?
-#   raise "security[additional_ports] is deprecated. please use security[ports] with reverted hash syntax."
-# end
-
 ports = node[:security][:default_ports].merge(node[:security][:ports])
 
-template "/etc/iptables.rules" do
-  source "iptables.rules.erb"
-  owner  "root"
-  group  "root"
-  mode   "0700"
+template '/etc/iptables.rules' do
+  source 'iptables.rules.erb'
+  owner  'root'
+  group  'root'
+  mode   '0700'
   variables(ports: ports)
 end
 
-bash "iptables-restore < /etc/iptables.rules"
-bash "iptables.rules to interfaces" do
-  user "root"
+bash 'iptables-restore < /etc/iptables.rules' do
+  user 'root'
+  code <<-EOH
+    iptables-restore < /etc/iptables.rules
+    EOH
+end
+
+bash 'iptables.rules to interfaces' do
+  user 'root'
   code <<-EOF
     echo "pre-up iptables-restore < /etc/iptables.rules" >> /etc/network/interfaces
   EOF
