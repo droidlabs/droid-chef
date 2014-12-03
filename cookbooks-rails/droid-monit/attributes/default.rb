89e14a1a783756b31abb09dd519166cfc5eac1b2
fadeev2010 <fadeev2010@gmail.com>
2014-12-03 09:13:28 +0300

    monit small fix

--- cookbooks-rails/droid-monit/attributes/default.rb
+++ cookbooks-rails/droid-monit/attributes/default.rb
@@ -13,7 +13,7 @@ default["monit"]["logfile"]                = 'syslog facility log_daemon'
 default["monit"]["poll_period"]            = 60
 default["monit"]["poll_start_delay"]       = 240
 
-default["monit"]["mail_format"]["subject"] = "[Monit] $SERVICE $EVENT"
+default["monit"]["mail_format"]["subject"] = "[Monit #{node.fqdn}] $SERVICE $EVENT"
 default["monit"]["mail_format"]["from"]    = "monit@#{node['fqdn']}"
 default["monit"]["mail_format"]["message"] = <<-EOS
 Monit $ACTION $SERVICE at $DATE on $HOST: $DESCRIPTION.
@@ -26,8 +26,8 @@ default["monit"]["address"]  = nil
 default["monit"]["ssl"]      = false
 default["monit"]["cert"]     = "/etc/monit/monit.pem"
 default["monit"]["allow"]    = []
-default["monit"]["username"] = "#{node[:deploy_user][:username]}"
-default["monit"]["password"] = "#{node[:deploy_user][:password]}"
+default["monit"]["username"] = "#{node[:deploy_user][:username]}" || 'guest_monit'
+default["monit"]["password"] = "#{node[:deploy_user][:password]}" || 'guest_monit'
 default["monit"]["ssh_port"] = 22
 
 default["monit"]["eventqueue"]["set"]     = true
