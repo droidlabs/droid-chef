1005d1f9288af81be45679061ca24dfcd181f41f
fadeev2010 <fadeev2010@gmail.com>
2014-12-06 19:05:05 +0300

    fix generate database.yml

--- cookbooks-rails/rails/definitions/setup_app_database.rb
+++ cookbooks-rails/rails/definitions/setup_app_database.rb
@@ -6,14 +6,14 @@ define :setup_app_database do
 
   database = app[:database] || node[:deploy_user][:database] || 'mysql'
   database_host = app[:database_host] || node[:deploy_user][:database_host] || 'localhost'
-  database_username = app[:database_username] || node[:deploy_user][:database_username] || 'root'
-  database_password = app[:database_password] || node[:deploy_user][:database_password] || ''
+  database_username = app[:app_user] || 'set_node_app_user'
+  database_password = app[:app_password] || 'set_node_app_password'
 
   if database.to_sym == :mongodb
     template "/data/#{app[:name]}/shared/config/mongoid.yml" do
       source 'mongoid.yml.erb'
-      owner app[:app_user] # deploy_username
-      group app[:app_user] # deploy_username
+      owner app[:app_user]
+      group app[:app_user]
       mode '0640'
       variables(
         app_name: app[:name],
@@ -26,8 +26,8 @@ define :setup_app_database do
     template_name = "database.#{database}.yml.erb"
     template "/data/#{app[:name]}/shared/config/database.yml" do
       source template_name
-      owner app[:app_user] # deploy_username
-      group app[:app_user] # deploy_username
+      owner app[:app_user]
+      group app[:app_user]
       mode '0640'
       variables(
         app_name: app[:name],
@@ -43,8 +43,8 @@ define :setup_app_database do
   ################# SECRET KEY BASE ###############################
   template "/data/#{app[:name]}/shared/config/secrets.yml" do
     source 'secrets.yml.erb'
-    owner app[:app_user] # deploy_username
-    group app[:app_user] # deploy_username
+    owner app[:app_user]
+    group app[:app_user]
     mode '0660'
     action :create_if_missing
     variables(
