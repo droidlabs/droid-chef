ccf326f82c6302e806cc6b0336fbde074fbd22df
fadeev2010 <fadeev2010@gmail.com>
2014-12-10 18:17:08 +0300

    fix for chef 12

--- cookbooks-rails/nginx/metadata.rb
+++ cookbooks-rails/nginx/metadata.rb
@@ -1,5 +1,4 @@
-maintainer "James Harton, Sociable Limited."
-maintainer_email "james@sociable.co.nz"
+name "nginx"
 license "MIT"
 description "Installs passenger via rubygems. Explicity uses system-wide RVM if present."
 version "0.0.1"
---
+++ cookbooks-rails/rails/metadata.rb
@@ -0,0 +1,2 @@
+name "rails"
+version "0.0.5"
