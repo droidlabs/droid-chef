f2c10b72ebc1c6c02c65c1062691e0cf671dac99
fadeev2010 <fadeev2010@gmail.com>
2015-01-14 20:33:49 +0300

    fix mysql cnf

--- cookbooks-rails/droid-mysql/recipes/default.rb
+++ cookbooks-rails/droid-mysql/recipes/default.rb
@@ -6,7 +6,19 @@
 
 include_recipe "mysql::client"
 include_recipe "mysql::server"
+
+template "/etc/mysql/conf.d/droid.cnf" do
+	source "droid.cnf.erb"
+	owner "root"
+	group "root"
+	mode "0644"
+end
+
 include_recipe "droid-mysql::applications"
+
+service "mysql" do
+	action [ :restart ]
+end
 
 #Monitoring cfg
 include_recipe 'droid-monit::mysql'
