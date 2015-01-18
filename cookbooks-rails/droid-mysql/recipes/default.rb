a64569e29c1812f3400b880c3a95368bcafed9fd
fadeev2010 <fadeev2010@gmail.com>
2015-01-18 14:01:02 +0300

    added notifies to mysql

--- cookbooks-rails/droid-mysql/recipes/default.rb
+++ cookbooks-rails/droid-mysql/recipes/default.rb
@@ -1,24 +1,19 @@
-# #
 # Cookbook Name:: droid-mysql //Wrapper cookbook
 # Attributes::mysql::default and droid-mysql
-#
-# Author:: Fadeev <fadeev2010@gmail.com>
+# Droid Labs LLC, Fadeev Sergey <fadeev2010@gmail.com>
 
 include_recipe "mysql::client"
 include_recipe "mysql::server"
 
 template "/etc/mysql/conf.d/droid.cnf" do
-	source "droid.cnf.erb"
-	owner "root"
-	group "root"
-	mode "0644"
+  source "droid.cnf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :reload, 'service[mysql]'
 end
 
 include_recipe "droid-mysql::applications"
-
-service "mysql" do
-	action [ :restart ]
-end
 
 #Monitoring cfg
 include_recipe 'droid-monit::mysql'
