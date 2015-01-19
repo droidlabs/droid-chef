aaffa289c0078ef9f46ac1aad6d76465f14d6940
fadeev2010 <fadeev2010@gmail.com>
2015-01-19 17:54:25 +0300

    fix mysql service bug

--- cookbooks-rails/droid-mysql/recipes/default.rb
+++ cookbooks-rails/droid-mysql/recipes/default.rb
@@ -5,12 +5,18 @@
 include_recipe "mysql::client"
 include_recipe "mysql::server"
 
+service "mysql" do
+  supports :restart => true, :start => true, :stop => true
+  provider Chef::Provider::Service::Init
+  action [ :start ]
+end
+
 template "/etc/mysql/conf.d/droid.cnf" do
   source "droid.cnf.erb"
   owner "root"
   group "root"
   mode "0644"
-  notifies :reload, 'service[mysql]'
+  notifies :restart, "service[mysql]"
 end
 
 include_recipe "droid-mysql::applications"
