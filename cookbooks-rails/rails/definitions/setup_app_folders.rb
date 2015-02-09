8a371e60ea5893364e3b38f8b604a5560d404305
fadeev2010 <fadeev2010@gmail.com>
2015-02-09 16:31:58 +0300

    fix logrotate rules
    Conflicts:
    	cookbooks-rails/nginx/recipes/daemon.rb

--- cookbooks-rails/rails/definitions/setup_app_folders.rb
+++ cookbooks-rails/rails/definitions/setup_app_folders.rb
@@ -42,7 +42,7 @@ define :setup_app_folders do
     include_recipe 'logrotate'
 
     logrotate_app "rails_#{app[:name]}" do
-      path ["/data/#{app[:name]}/shared/log/*.log", "/data/#{app[:name]}/shared/log/**/*.log"]
+      path ["/data/#{app[:name]}/shared/log/*.log", "/data/#{app[:name]}/shared/log/**/*.log", "/data/#{app[:name]}/shared/log/**/*.output"]
       frequency 'daily'
       rotate 14
       su 'root'
